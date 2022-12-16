091f64dd095904648c5402874362e3e5c2c521db
Alexandra Knopf <xyious@gmail.com>
2022-12-15 20:01:13 -0800

    make creating posts actually work, get author name on homepage

--- controllers/Blog.cc
+++ controllers/Blog.cc
@@ -1,15 +1,17 @@
+#include <iostream>
 #include "Website.h"
 #include "Blog.h"
 
 void Blog::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
     std::vector<std::string> keywords;
+    std::string title, subtitle, tags, content, author, isBlog = "0";
     if (req->getMethod() == HttpMethod::Post) {
-        std::string title, subtitle, tags, content, author, isBlog;
         auto params = req->getParameters();
         for (auto param : params) {
             std::string key = std::get<0>(param);
             std::string value = std::get<1>(param);
+            std::cout << key << " " << value << std::endl;
             if (key == "title") {
                 title = value;
             } else if (key == "subtitle") {
@@ -18,22 +20,45 @@ void Blog::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void 
                 tags = value;
             } else if (key == "content") {
                 content = value;
+            } else if (key == "author") {
+                author = value;
             } else if (key == "isBlog") {
-                isBlog = value;
+                if (value == "on") {
+                    isBlog = "1";
+                }
             }
         }
         auto clientPtr = drogon::app().getDbClient("dwebsite");
         std::string query = "INSERT INTO dwblog (title, subtitle, content, isBlog, author) VALUES ($1, $2, $3, $4, $5)";
-        auto result = clientPtr->execSqlSync(query, title, subtitle, content, isBlog, 0);       // TODO: obviously fix this, need to make isBlog and author dropdown once I figure out how to create more users
-        auto site = new website(keywords, "en", "Creating Post !", "Creating Post !");
-        callback(site->getPage());
-        return;
-    } else {
-        std::string form = "<form action='' method='post'><label for='title'>Title:</label><input type='text' name='Title' required><br><label for='subtitle'>Subtitle:</label><input type='text' name='subtitle'><br>\
-        <label for='author'>Author:</label><input type='text' name='author'><br><label for='isBlog'>isBlog ?:</label><input type='text' name='isBlog'><br>\
-        <label for='content'>Content:</label><input type='textarea' name='content'><br><input type='submit' value='submit'><form>";
-        auto site = new website(keywords, "en", "Create Post", form);
-        callback(site->getPage());
-        return;
+        try {
+            auto result = clientPtr->execSqlSync(query, title, subtitle, content, isBlog, author);       // TODO: obviously fix this, need to make isBlog and author dropdown once I figure out how to create more users
+            auto site = new website(keywords, "en", "Creating Post !", "Creating Post !");
+            callback(site->getPage());
+            return;
+        }
+        catch (std::exception const& e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
+    auto clientPtr = drogon::app().getDbClient("dwebsite");
+    std::string query = "SELECT id, name FROM dwUsers";
+    auto result = clientPtr->execSqlSync(query);
+    std::string form = "<form action='' method='post'><label for='title'>Title:</label><input type='text' name='title' value='";
+    form.append(title);
+    form.append("' required><br><label for='subtitle'>Subtitle:</label><input type='text' name='subtitle' value='");
+    form.append(subtitle);
+    form.append("'><br><label for='author'>Author:</label><select name='author'>");
+    for (auto row : result) {
+        form.append("<option value='");
+        form.append(row["id"].as<std::string>());
+        form.append("'>");
+        form.append(row["name"].as<std::string>());
+        form.append("</option>");
+    }
+    form.append("</select><br><label for='isBlog'>isBlog ?:</label><input type='checkbox' id='isBlog' name='isBlog'><br><label for='content'>Content:</label><input type='textarea' name='content' value='");
+    form.append(content);
+    form.append("'><br><input type='submit' value='submit'><form>");
+    auto site = new website(keywords, "en", "Create Post", form);
+    callback(site->getPage());
+    return;
 }
--- controllers/SiteController.cc
+++ controllers/SiteController.cc
@@ -8,14 +8,14 @@ void SiteController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::func
 {
     std::vector<std::string> keywords;
     auto clientPtr = drogon::app().getDbClient("dwebsite");
-    std::string query = "SELECT title, subtitle, content, author, create_timestamp, tags FROM dwBlog WHERE isBlog=1 ORDER BY create_timestamp DESC LIMIT 3";
+    std::string query = "SELECT dwBlog.title, dwBlog.subtitle, dwBlog.content, dwUsers.name, dwBlog.create_timestamp, dwBlog.tags FROM dwBlog, dwUsers WHERE dwBlog.author = dwUsers.id AND isBlog=1 ORDER BY create_timestamp DESC LIMIT 3";
     auto result = clientPtr->execSqlSync(query);
     std::string content, title;
     for (auto row : result) {
         if (title.empty()) {
             title = row["title"].as<std::string>();
         }
-        content.append(website::getContent(row["title"].as<std::string>(), row["subtitle"].as<std::string>(), row["content"].as<std::string>(), row["author"].as<std::string>(), row["create_timestamp"].as<std::string>(), row["tags"].as<std::string>()));
+        content.append(website::getPost(row["title"].as<std::string>(), row["subtitle"].as<std::string>(), row["content"].as<std::string>(), row["name"].as<std::string>(), row["create_timestamp"].as<std::string>(), row["tags"].as<std::string>()));
         std::cout << content << std::endl;
     }
     auto site = new website(keywords, "en", title, content);
