58b879cd3267c4514642c9144fade3d78989f46f
Alexandra Knopf <xyious@gmail.com>
2023-01-13 15:56:38 -0800

    fix login page

--- controllers/UsersController.h
+++ controllers/UsersController.h
@@ -7,7 +7,7 @@ class UsersController : public drogon::HttpController<UsersController>
 {
   public:
     METHOD_LIST_BEGIN
-    METHOD_ADD(UsersController::login, "/login", drogon::Get, drogon::Post);
+    METHOD_ADD(UsersController::login, "/login", drogon::Get);
     METHOD_LIST_END
 
     void login(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback);
