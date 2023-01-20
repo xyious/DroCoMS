12721a252ebcf1f0bd64d2ec8d20848082bd9b1e
Alexandra Knopf <xyious@gmail.com>
2023-01-19 20:18:42 -0800

    add favicon

--- helpers/Constants.h
+++ helpers/Constants.h
@@ -3,5 +3,5 @@
 const std::string DOCTYPE = "<!doctype html>";
 const std::string HTMLTAGEN = "<html lang=\"en\"><head>";
 const std::string HTMLTAGDE = "<html lang=\"de\"><head>";
-const std::string BODYTAG = "</head><body>";
+const std::string BODYTAG = "<link rel='icon' type='image/png' href='favicon.png'/></head><body>";
 const std::string ENDTAG = "</body></html>";
