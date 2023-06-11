5b4aadf640eb104303b376fc8e53d2278b29cbe2
Alexandra Knopf <xyious@gmail.com>
2023-06-10 20:50:22 -0700

    change InstallDb to still create tables even if database exists

--- controllers/InstallDb.cc
+++ controllers/InstallDb.cc
@@ -24,43 +24,39 @@ void InstallDb::asyncHandleHttpRequest(const drogon::HttpRequestPtr& req, std::f
 		LOG_ERROR << exc.what();
     }
     std::string output = "";
-    if (dbExists) {
-        auto site = new website(keywords, "en", "Create Database", "Database already exists !", "", "");
-        callback(site->getPage());
-        return;
-    } else {
-        if (req->getMethod() == drogon::HttpMethod::Post) {
-            LOG_TRACE << "Post InstallDb";
-            std::string username, email, name, token;
-            auto params = req->getParameters();
-            for (auto param : params) {
-                std::string key = std::get<0>(param);
-                std::string value = std::get<1>(param);
-                if (key == "name") {
-                    name = value;
-                } else if (key == "username") {
-                    username = value;
-                } else if (key == "email") {
-                    email = value;
-                }
-                LOG_TRACE << "param " << key << ", value: " << value;
+    if (req->getMethod() == drogon::HttpMethod::Post) {
+        LOG_TRACE << "Post InstallDb";
+        std::string username, email, name, token;
+        auto params = req->getParameters();
+        for (auto param : params) {
+            std::string key = std::get<0>(param);
+            std::string value = std::get<1>(param);
+            if (key == "name") {
+                name = value;
+            } else if (key == "username") {
+                username = value;
+            } else if (key == "email") {
+                email = value;
             }
-            query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Blog (post_id SERIAL PRIMARY KEY, url VARCHAR(255), title VARCHAR(255), subtitle VARCHAR(255), language CHAR(5), content text, author INT, category INT, isBlog INT, create_timestamp timestamp DEFAULT current_timestamp, edit_timestamp timestamp);";
-            output = "Installing Database....<br>";
-            output.append("Creating blog table....<br>");
-            auto result = clientPtr->execSqlSync(query);
-            output.append(".... done<br>Creating Tags table....<br>");
-            query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Tags (tag_id SERIAL PRIMARY KEY, tag VARCHAR(255), description VARCHAR(255));";
-            result = clientPtr->execSqlSync(query);
-            output.append(".... done<br>Creating AssignedTags table....<br>");
-            query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "TagsAssigned (tag_id INT, post_id INT);";
-            result = clientPtr->execSqlSync(query);
-            output.append(".... done<br>Creating users table....<br>");
-            query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Users (id SERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE, email VARCHAR(255), name VARCHAR(255), token VARCHAR(100), expiration BIGINT, can_post INT, create_timestamp timestamp DEFAULT current_timestamp);";
-            result = clientPtr->execSqlSync(query);
-            output.append(".... done<br>Creating categories table ....<br>");
-            query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Categories (id SERIAL PRIMARY KEY, name VARCHAR(255), parent INT, description TEXT, language CHAR(5), isBlog INT, isExternal INT);";
-            result = clientPtr->execSqlSync(query);
+            LOG_TRACE << "param " << key << ", value: " << value;
+        }
+        query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Blog (post_id SERIAL PRIMARY KEY, url VARCHAR(255), title VARCHAR(255), subtitle VARCHAR(255), language CHAR(5), content text, author INT, category INT, isBlog INT, create_timestamp timestamp DEFAULT current_timestamp, edit_timestamp timestamp);";
+        output = "Installing Database....<br>";
+        output.append("Creating blog table....<br>");
+        auto result = clientPtr->execSqlSync(query);
+        output.append(".... done<br>Creating Tags table....<br>");
+        query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Tags (tag_id SERIAL PRIMARY KEY, tag VARCHAR(255), description VARCHAR(255));";
+        result = clientPtr->execSqlSync(query);
+        output.append(".... done<br>Creating AssignedTags table....<br>");
+        query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "TagsAssigned (tag_id INT, post_id INT);";
+        result = clientPtr->execSqlSync(query);
+        output.append(".... done<br>Creating users table....<br>");
+        query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Users (id SERIAL PRIMARY KEY, username VARCHAR(255) UNIQUE, email VARCHAR(255), name VARCHAR(255), token VARCHAR(100), expiration BIGINT, can_post INT, create_timestamp timestamp DEFAULT current_timestamp);";
+        result = clientPtr->execSqlSync(query);
+        output.append(".... done<br>Creating categories table ....<br>");
+        query = "CREATE TABLE IF NOT EXISTS " + helpers::TablePrefix + "Categories (id SERIAL PRIMARY KEY, name VARCHAR(255), parent INT, description TEXT, language CHAR(5), isBlog INT, isExternal INT);";
+        result = clientPtr->execSqlSync(query);
+        if (!dbExists) {
             output.append(".... done<br>Creating category....<br>");
             query = "INSERT INTO " + helpers::TablePrefix + "Categories (name, description, language, isBlog, isExternal) VALUES ('main', 'This is where literally everything goes', 'en-US', 1, 0);";
             result = clientPtr->execSqlSync(query);
@@ -70,20 +66,22 @@ void InstallDb::asyncHandleHttpRequest(const drogon::HttpRequestPtr& req, std::f
             unsigned char hash[SHA512_DIGEST_LENGTH];
             EVP_Digest(password.c_str(),password.size(),hash,NULL,EVP_sha512(),NULL);
             token = drogon::utils::base64Encode(hash, SHA512_DIGEST_LENGTH, true);
-            unsigned long long expiration = trantor::Date::date().microSecondsSinceEpoch() + (365L * 24 * 60 * 60 * 1000);         // The first user gets to stay logged in for 1 year (because it's me and I make good decisions)
+            unsigned long long expiration = trantor::Date::date().microSecondsSinceEpoch() + (365L * 24 * 60 * 60 * 1000000);         // The first user gets to stay logged in for 1 year (because it's me and I make good decisions)
             req->session()->insert("loginTimeout",  expiration);
             result = clientPtr->execSqlSync(query, username, email, name, token, expiration, 1);
             LOG_TRACE << "username: " << username << ", email: " << email << ", name: " << name << ", token: " << token << ", expiration: " << expiration;
             output.append(".... done !!!!");
-            auto site = new website(keywords, "en", "Installing Database", output);
-            callback(site->getPage());
-            return;
         } else {
-            LOG_TRACE << "Get InstallDb";
-            std::string form = "<form action='' method='post'><label for='username'>Username:</label><input type='text' name='username' required><br><label for='email'>Email:</label><input type='email' name='email' required><br><label for='name'>Name:</label><input type='text' name='name'><br><input type='submit' value='submit'><form>";
-            auto site = new website(keywords, "en", "Create Database", form);
-            callback(site->getPage());
-            return;
+            output.append("Database exists, skipping user and category creation");
         }
+        auto site = new website(keywords, "en", "Installing Database", output);
+        callback(site->getPage());
+        return;
+    } else {
+        LOG_TRACE << "Get InstallDb";
+        std::string form = "<form action='' method='post'><label for='username'>Username:</label><input type='text' name='username' required><br><label for='email'>Email:</label><input type='email' name='email' required><br><label for='name'>Name:</label><input type='text' name='name'><br><input type='submit' value='submit'><form>";
+        auto site = new website(keywords, "en", "Create Database", form);
+        callback(site->getPage());
+        return;
     }
 }
